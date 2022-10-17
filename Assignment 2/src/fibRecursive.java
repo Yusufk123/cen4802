4b20bad2c1e57977b36940a9c4aa889cf2c9d072
Yusufk123 <yusufk1230@gmail.com>
2022-10-16 23:01:31 -0400

    added Junit

--- Assignment 2/src/fibRecursive.java
+++ Assignment 2/src/fibRecursive.java
@@ -1,3 +1,6 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * Returns the number located at the nth term of the Fibonacci Sequence using a recursive method.
  * @author Yusuf Kounnichi
@@ -8,11 +11,21 @@ public class fibRecursive {
 	 * @param n The term to count to.
 	 * @return Returns the calculated number.
 	 */
+
 	public static int fibR(int n) {
 		if (n <= 1) {
 			return n;
 		} else {
 			return fibR(n - 1)+fibR(n - 2);
 		}
+	}
+
+	@Test
+	void fibRTest10(){
+		assertEquals(55, fibR(10));
+	}
+	@Test
+	void fibRTest21(){
+		assertEquals(10946, fibR(21));
 	}
 }
